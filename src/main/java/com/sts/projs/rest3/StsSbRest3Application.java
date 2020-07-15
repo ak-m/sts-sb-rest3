9f054d4e6c5bc32913ba8aa0ac17205fcf24d7b9
ak-m <ak.ghub.m@gmail.com>
2020-07-15 19:04:25 -0400

    step1: Initial spring boot app with basic project structure

---
+++ src/main/java/com/sts/projs/rest3/StsSbRest3Application.java
@@ -0,0 +1,13 @@
+package com.sts.projs.rest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsSbRest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsSbRest3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sts/projs/rest3/StsSbRest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sts.projs.rest3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsSbRest3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
